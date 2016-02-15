fe9c71b6c9936392f80a1cdd65c51cff10a3787d
Florent Vilmart <florentvilmart@me.com>
2016-02-15 15:46:55 -0500

    0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # parse-server-urban-airship
+[![Build Status](https://travis-ci.org/flovilmart/parse-server-urban-airship.svg?branch=master)](https://travis-ci.org/flovilmart/parse-server-urban-airship)
+[![codecov.io](https://codecov.io/github/flovilmart/parse-server-urban-airship/coverage.svg?branch=master)](https://codecov.io/github/flovilmart/parse-server-urban-airship?branch=master)
+
+
 Urban Airship adapter for parse-server
+
+
+## Usage
+
+install in your project with
+
+`npm install --save parse-server-urban-airship`
+
+Configure:
+
+
+```
+var UAPushAdapter = require('parse-server-urban-airship');
+var pushAdapter = new UAPushAdapter({
+  key: 'YOUR APP KEY',
+  secret: 'YOUR APP SECRET',
+  masterSecret: 'YOUR APP MASTER SECRET'
+});
+
+
+var server = new ParseServer({
+  ...
+  push: {
+    adapter: pushAdapter
+  }
+}
+
+```
---
+++ spec/UAPushAdapter.spec.js
@@ -0,0 +1,111 @@
+var UAPushAdapter = require('../index');
+
+var pushConfig = {
+  key: "aKey",
+  secret:"aSecret",
+  masterSecret: "aMasterSecret"
+};
+
+describe('UAPushAdapter', () => {
+  it('can be initialized', (done) => {
+
+    var adapter = new UAPushAdapter(pushConfig);
+
+    expect(adapter.getValidPushTypes()).toEqual(['ios', 'android', 'winrt', 'winphone', 'dotnet']);
+    done();
+  });
+
+  it('can generate requests', (done) => {
+    // Mock installations
+    var installations = [
+      {
+        deviceType: 'android',
+        deviceToken: 'androidToken'
+      },
+      {
+        deviceType: 'ios',
+        deviceToken: 'iosToken'
+      },
+      {
+        // Invalid 
+        deviceType: 'win',
+        deviceToken: 'winToken'
+      },
+      {
+        // invalid
+        deviceType: 'android',
+        deviceToken: undefined
+      }
+    ];
+
+    var requests = UAPushAdapter.createRequests({data: {}}, installations);
+    expect(requests.length).toBe(2);
+    done();
+  });
+  
+  it('fails to generate requests with empty', (done) => {
+    // Mock installations
+    var installations = [
+      {
+        deviceType: 'android',
+        deviceToken: 'androidToken'
+      },
+      {
+        deviceType: 'ios',
+        deviceToken: 'iosToken'
+      },
+      {
+        // Invalid 
+        deviceType: 'win',
+        deviceToken: 'winToken'
+      },
+      {
+        // invalid
+        deviceType: 'android',
+        deviceToken: undefined
+      }
+    ];
+
+    var requests = UAPushAdapter.createRequests({}, installations);
+    expect(requests.length).toBe(0);
+    done();
+  });
+
+  it("can post the correct data", (done) => {
+
+    var adapter = new UAPushAdapter(pushConfig)
+    
+    var installations = [
+      {
+        deviceType: 'android',
+        deviceToken: 'androidToken'
+      },
+      {
+        deviceType: 'ios',
+        deviceToken: 'iosToken'
+      },
+      {
+        deviceType: 'win',
+        deviceToken: 'winToken'
+      },
+      {
+        deviceType: 'android',
+        deviceToken: undefined
+      }
+    ];
+    
+    var data = {'data':{
+  		'title': 'Example title',
+  		'alert': 'Example content',
+  		'content-available':1,
+  		'misc-data': 'Example Data'
+  	}}
+    
+    adapter.send(data, installations).then(function(res){
+      fail('Should not succeed');
+      done();
+    }, function(err){
+      done();
+    });
+  });
+});
