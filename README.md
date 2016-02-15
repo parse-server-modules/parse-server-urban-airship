2bfa5e93fff9812c2dc46f0ef6213715f3dcbf59
Florent Vilmart <florentvilmart@me.com>
2016-02-15 15:46:55 -0500

    0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # parse-server-urban-airship
 Urban Airship adapter for parse-server
+
+
+## Usage
+
+install in your project with
+
+`npm install --save parse-server-urban-airship`
+
+Configure:
+
+
+```
+var UAPushAdapter = require('parse-server-urban-airship');
+var pushAdapter = new UAPushAdapter({
+  key: 'YOUR APP KEY',
+  secret: 'YOUR APP SECRET',
+  masterSecret: 'YOUR APP MASTER SECRET'
+});
+
+
+var server = new ParseServer({
+  ...
+  push: {
+    adapter: pushAdapter
+  }
+}
+
+```
---
+++ index.js
@@ -0,0 +1,84 @@
+"use strict";
+// ParsePushAdapter is the default implementation of
+// PushAdapter, it uses GCM for android push and APNS
+// for ios push.
+
+const typeToAudience = {
+  "ios": "device_token",
+  "android": "apid",
+  "winrt": "wns",
+  "winphone": "mpns",
+  "dotnet": "wns"
+}
+
+const Parse = require('parse/node').Parse;
+var UrbanAirshipPush = require('urban-airship-push');
+
+function UrbanAirshipPushAdapter(pushConfig) {
+  this.https = require('https');
+
+  this.validPushTypes = ['ios', 'android',  'winrt', 'winphone', 'dotnet'];
+  this.senderMap = {};
+
+  pushConfig = pushConfig || {};
+  this.config = {};
+  this.config.key = pushConfig.key;
+  this.config.secret = pushConfig.secret;
+  this.config.masterSecret = pushConfig.masterSecret;
+  
+  this.UA = new UrbanAirshipPush(this.config);
+}
+
+/**
+ * Get an array of valid push types.
+ * @returns {Array} An array of valid push types
+ */
+UrbanAirshipPushAdapter.prototype.getValidPushTypes = function() {
+  return this.validPushTypes;
+}
+
+UrbanAirshipPushAdapter.prototype.send = function(data, installations) {
+  console.log("Sending notification to "+installations.length+" devices.")
+
+  let requests = UrbanAirshipPushAdapter.createRequests(data, installations);
+  
+  var promise = new Parse.Promise();
+  this.UA.push.send(requests, function(err, res){
+    if (err) {
+        promise.reject(err);
+    } else {
+      promise.resolve(res);
+    }
+  });
+  return promise;
+}
+
+UrbanAirshipPushAdapter.createRequests = function(data, installations) {
+  return installations.map(function(installation){
+    let type = typeToAudience[installation.deviceType];
+    if (!type) {
+      return;
+    }
+    let deviceToken = installation.deviceToken;
+    if (!deviceToken) {
+      return;
+    }
+    if (!data.data) {
+      return;
+    }
+    let audience = {};
+    audience[type] = deviceToken;
+    var specificData = {};
+    specificData[installation.deviceType] = data.data;
+    
+    return {
+      audience: audience,
+      notification: specificData, 
+      device_types: "all"
+    }
+  }).filter(function(obj){
+    return obj !== undefined;
+  });
+}
+
+module.exports = UrbanAirshipPushAdapter;
